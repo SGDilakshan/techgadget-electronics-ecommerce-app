25017f59e7155af646ef39c87555caec3078e4cd
SGDilakshan <sgd.dilakshan@gmail.com>
2025-09-07 22:25:35 +0530

    update the formatters.js

---
+++ frontend/src/utils/formatters.js
@@ -0,0 +1,14 @@
+export const formatPrice = (price) => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  }).format(price)
+}
+
+export const formatDate = (dateString) => {
+  return new Date(dateString).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  })
+}
