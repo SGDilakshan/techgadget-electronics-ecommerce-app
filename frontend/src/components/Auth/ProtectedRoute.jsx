a4f10695d5ced8a593d83dae2f80a880ad6a3fed
SGDilakshan <sgd.dilakshan@gmail.com>
2025-09-07 22:34:06 +0530

    update the ProtectedRoute.jsx

---
+++ frontend/src/components/Auth/ProtectedRoute.jsx
@@ -0,0 +1,20 @@
+import { useAuth } from '../../hooks/useAuth'
+import LoadingSpinner from '../UI/LoadingSpinner'
+import { Navigate, useLocation } from 'react-router-dom'
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, isLoading } = useAuth()
+  const location = useLocation()
+
+  if (isLoading) {
+    return <LoadingSpinner />
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" state={{ from: location }} replace />
+  }
+
+  return children
+}
+
+export default ProtectedRoute
